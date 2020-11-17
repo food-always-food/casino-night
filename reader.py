ebf65d7e5316b3e62b45fd6b65e9185cdd123945
food-always <duncan@thompsongroup.io>
2020-11-17 20:35:20 +0000

    added first read and write scripts

---
+++ reader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+from time import sleep
+
+reader = SimpleMFRC522()
+
+while True:
+	try:
+	        id, text = reader.read()
+	        #print(id)
+	        os.system('clear')
+	        print(text)
+	        sleep(1)
+	        
+	finally:
+		GPIO.cleanup()
+			
+	
